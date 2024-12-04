379d3a2d611d561662828b2ffb75e1a4be3a6f9f
mobin afshari <mma137813781378@gmail.com>
2024-12-04 23:37:30 +0330

    ready to add to netlify

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,12 @@
+const { DateTime } = require("luxon");
+
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("./src/style.css");
-    eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addPassthroughCopy("./src/style.css");
+  eleventyConfig.addPassthroughCopy("./src/assets");
+
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
   return {
     dir: {
       input: "src",
@@ -8,7 +14,7 @@ module.exports = function (eleventyConfig) {
     },
     serverOptions: {
       host: "0.0.0.0",
-      port: 8000, 
+      port: 8000,
     },
   };
 };
